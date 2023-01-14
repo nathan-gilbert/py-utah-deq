3c44f3af79c67e6da1179fac2db0826b35b12873
Nathan Gilbert <nathan.gilbert@gmail.com>
2023-01-14 16:34:16 -0700

    Adding abstract method

--- src/py_aqi/plugins/data_sources/utah_deq.py
+++ src/py_aqi/plugins/data_sources/utah_deq.py
@@ -1,6 +1,8 @@
 """
 Class for implementing a data source for Utah's Dept of Env Quality
 """
+from typing import Any
+
 from src.py_aqi.plugins.data_sources.data_source import DataSource
 
 
@@ -8,3 +10,6 @@ class UtahDEQ(DataSource):
     """
     Implementation of a DataSource from Utah's DEQ
     """
+
+    def get_current_aqi(self) -> Any:
+        pass
